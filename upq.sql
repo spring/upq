dc10aff231fd77c0cd4c9e0371b0cf6b44a02bc4
abma <spam@abma.de>
2011-05-16 05:38:52 +0200

    update upq.sql

--- upq.sql
+++ upq.sql
@@ -19,11 +19,12 @@ CREATE TABLE IF NOT EXISTS `upqueue` (
   `jobid` int(11) NOT NULL AUTO_INCREMENT,
   `jobname` varchar(255) NOT NULL COMMENT 'job module name',
   `state` varchar(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL COMMENT 'new/running/done',
-  `pickle_blob` blob NOT NULL COMMENT 'obj state saved with pickle',
+  `pickle_blob` text NOT NULL COMMENT 'obj state saved with json',
   `result` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'UpqJob.result[''success'']',
   `result_msg` varchar(255) NOT NULL DEFAULT 'New' COMMENT 'UpqJob.result[msg]',
   `ctime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'job create time',
   `start_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'job start time',
   `end_time` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'job end time',
   PRIMARY KEY (`jobid`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=100 ;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
