a7a58f106e82940bc2f657cb0a5fbca3f3790e7e
dansan <daniel@admin-box.com>
2011-05-25 22:39:16 +0200

    fix indentation, fix hash() result usage

--- jobs/hash.py
+++ jobs/hash.py
@@ -12,6 +12,8 @@
 #
 
 import hashlib
+import os.path
+
 from upqjob import UpqJob
 from upqdb import UpqDB
 from upqdb import UpqDBIntegrityError
@@ -23,41 +25,41 @@ class Hash(UpqJob):
 	"""
 	#TODO: allow recheck hash if last check of hash is older than one year (time taken from config)
 	def check(self):
-		results = UpqDB().query("SELECT filepath,md5 FROM files as f LEFT JOIN filehash as h ON f.fid=h.fid WHERE f.fid=%d "% int(self.jobdata['fid']))
-		if results.rowcount>1:
-			self.msg="Integry check failed, more than 1 file to hash in result"
+		results = UpqDB().query("SELECT filepath,md5 FROM files as f LEFT JOIN filehash as h ON f.fid=h.fid WHERE f.fid=%d " % int(self.jobdata['fid']))
+		if results.rowcount > 1:
+			self.msg = "Integry check failed, more than 1 file to hash in result"
 			return False
 		for res in results:
-			if len(res['md5'])>0: #md5 already present, don't hash again
-				self.msg="MD5 already present: " + res['md5']
+			if len(res['md5']) > 0: #md5 already present, don't hash again
+				self.msg = "MD5 already present: " + res['md5']
 				return False
 			self.enqueue_job()
 			if not os.path.isfile(self.jobcfg['path'] + res['filepath']):
-				self.msg="File not found "+self.jobcfg['path'] + res['filepath']
+				self.msg = "File not found " + self.jobcfg['path'] + res['filepath']
 				return False;
 			return True
-		self.msg="File not found"
+		self.msg = "File not found"
 		return False
 
 	def run(self):
 		"""
 			class Hash must be initialized with fileid!
 		"""
-		fid=self.jobdata['fid']
-		results = UpqDB().query("SELECT * FROM files f LEFT JOIN filehash h ON f.fid=h.fid WHERE f.fid=%d  "% int(fid))
+		fid = self.jobdata['fid']
+		results = UpqDB().query("SELECT * FROM files f LEFT JOIN filehash h ON f.fid=h.fid WHERE f.fid=%d  " % int(fid))
 		for res in results:
 			file = self.jobcfg['path'] + res['filepath']
-			md5, sha1, sha256 = hash(file)
+			hashes = self.hash(file)
 
 			try:
-				UpqDB().insert("filehash", { "fid": self.jobdata['fid'], "md5": md5, "sha1": sha1, "sha256": sha256 })
+				UpqDB().insert("filehash", { "fid": self.jobdata['fid'], "md5": hashes['md5'], "sha1": hashes['sha1'], "sha256": hashes['sha256'] })
 			except UpqDBIntegrityError:
-				self.msg="Hash already exists in db, not updating"
+				self.msg = "Hash already exists in db, not updating"
 				return True
 		return True
 
-    def hash(self, filename):
-        """
+	def hash(self, filename):
+		"""
         Calculate hashes (md5, sha1, sha256) of a given file
 
         filename is absolute path to file
@@ -65,27 +67,24 @@ class Hash(UpqJob):
         returns: {'md5': 'x', 'sha1': 'y', 'sha256': 'z'}
         """
 
-        logger = log.getLogger()
+		md5 = hashlib.md5()
+		sha1 = hashlib.sha1()
+		sha256 = hashlib.sha256()
 
-        md5 = hashlib.md5()
-        sha1 = hashlib.sha1()
-        sha256 = hashlib.sha256()
+		try:
+			fd = open(filename, "rb", 4096)
+		except IOError, ex:
+			msg = "Unable to open '%s' for reading: '%s'." % (filename, ex)
+			self.logger.error(msg)
+			raise Exception(msg)
+		
+		while True:
+			data = fd.read(4096)
+			if not data: break
+			md5.update(data)
+			sha1.update(data)
+			sha256.update(data)
 
-        try:
-            fd = open(filename, "rb", 4096)
-        except IOError, ex:
-            msg = "Unable to open '%s' for reading: '%s'." % (filename, ex)
-            logger.error(msg)
-            raise Exception(msg)
-        while True:
-            data = fd.read(4096)
-            if not data: break
-            md5.update(data)
-            sha1.update(data)
-            sha256.update(data)
+		fd.close()
 
-        fd.close()
-
-        return {'md5': md5.hexdigest(), 'sha1': sha1.hexdigest(), 'sha256': sha256.hexdigest()}
-
-    
+		return {'md5': md5.hexdigest(), 'sha1': sha1.hexdigest(), 'sha256': sha256.hexdigest()}
