4092b22e1c253c871566ac18a03896345541f146
abma <spam@abma.de>
2011-05-25 16:17:40 +0200

    fix job extract_metadata

--- jobs/extract_metadata.py
+++ jobs/extract_metadata.py
@@ -9,10 +9,8 @@
 #called with fileid, extracts/inserts metadata
 #calls upload
 
-import upqtask
 from upqjob import UpqJob
 
-from unitsync import unitsync as unitsyncpkg
 import sys
 import os
 import ctypes
@@ -22,12 +20,18 @@ import getopt
 import base64
 
 import sys
-sys.path.append('metalink')
+sys.path.append('jobs')
+from unitsync import unitsync as unitsyncpkg
+sys.path.remove('jobs')
+sys.path.append('jobs/metalink')
 import metalink
+sys.path.remove('jobs/metalink')
 
 from xml.dom import minidom
 
 class Extract_metadata(UpqJob):
+	#TODO: make it possible to hash only a single file
+	#this could be done by creating an empty directory + symlink the file there
 	def run(self):
 		unitsync=self.jobcfg['unitsync']
 		outputpath=self.jobcfg['outputpath']
@@ -40,24 +44,24 @@ class Extract_metadata(UpqJob):
 		usync.Init(True,1)
 		mapcount = usync.GetMapCount()
 		gamescount = usync.GetPrimaryModCount()
-		createdict(usync,gamescount, mapcount)
+		self.createdict(usync,gamescount, mapcount)
 		for i in range(0, mapcount):
 			maparchivecount = usync.GetMapArchiveCount(usync.GetMapName(i)) # initialization for GetMapArchiveName()
 			filename = os.path.basename(usync.GetMapArchiveName(0))
 			archivepath=usync.GetArchivePath(filename)+filename
 			print "["+str(i) +"/"+ str(mapcount)+ "] extracting data from "+filename
 			springname = usync.GetMapName(i)
-			dumpmap(usync, springname, outputpath, filename,i)
-			writeMapXmlData(usync, springname, i, outputpath +"/" +filename+".metadata.xml",maparchivecount, archivepath)
-			create_torrent(archivepath, outputpath +"/" +filename+".torrent")
+			self.dumpmap(usync, springname, outputpath, filename,i)
+			self.writeMapXmlData(usync, springname, i, outputpath +"/" +filename+".metadata.xml",maparchivecount, archivepath)
+			self.create_torrent(archivepath, outputpath +"/" +filename+".torrent")
 		for i in range (0, gamescount):
 			springname=usync.GetPrimaryModName(i)
 			filename=usync.GetPrimaryModArchive(i)
 			archivepath=usync.GetArchivePath(filename)+filename
 			print "["+str(i) +"/"+ str(gamescount)+ "] extracting data from "+filename
 			gamearchivecount=usync.GetPrimaryModArchiveCount(i) # initialization for GetPrimaryModArchiveList()
-			writeGameXmlData(usync, springname, i, outputpath + "/" + filename + ".metadata.xml", gamearchivecount, archivepath)
-			create_torrent(archivepath, outputpath +"/" +filename+".torrent")
+			self.writeGameXmlData(usync, springname, i, outputpath + "/" + filename + ".metadata.xml", gamearchivecount, archivepath)
+			self.create_torrent(archivepath, outputpath +"/" +filename+".torrent")
 		print "Parsed "+ str(gamescount) + " games, " + str(mapcount) + " maps"
 
 	#calls extract metadata script
@@ -65,7 +69,7 @@ class Extract_metadata(UpqJob):
 
 	springcontent = [ 'bitmaps.sdz', 'springcontent.sdz', 'maphelper.sdz', 'cursors.sdz' ]
 
-	def getXmlData(doc, parent, element, value):
+	def getXmlData(self, doc, parent, element, value):
 		node = doc.createElement(element)
 		value = str(value)
 		value = value.decode('utf-8','replace')
@@ -73,62 +77,62 @@ class Extract_metadata(UpqJob):
 		node.appendChild(subnode)
 		parent.appendChild(node)
 
-	def getMapPositions(usync,doc, idx, Map):
+	def getMapPositions(self, usync,doc, idx, Map):
 		positions = doc.createElement("Positions")
 		startpositions = usync.GetMapPosCount(idx)
 		for i in range(0, startpositions):
 			startpos=doc.createElement("StartPos")
-			getXmlData(doc, startpos, "X", str(usync.GetMapPosX(idx, i)))
-			getXmlData(doc, startpos, "Z", str(usync.GetMapPosZ(idx, i)))
+			self.getXmlData(doc, startpos, "X", str(usync.GetMapPosX(idx, i)))
+			self.getXmlData(doc, startpos, "Z", str(usync.GetMapPosZ(idx, i)))
 			positions.appendChild(startpos)
 		Map.appendChild(positions)
 
-	def getMapDepends(usync,doc,idx,Map,maparchivecount):
+	def getMapDepends(self, usync,doc,idx,Map,maparchivecount):
 		for j in range (1, maparchivecount): # get depends for file, idx=0 is filename itself
 			deps=os.path.basename(usync.GetMapArchiveName(j))
 			node = doc.createElement("Depends")
-			if not deps in springcontent:
-				getXmlData(doc, node, "Depend", str(deps))
+			if not deps in self.springcontent:
+				self.getXmlData(doc, node, "Depend", str(deps))
 		Map.appendChild(node)
-	def getMapResources(usync,doc,idx,Map, maparchivecount):
+	def getMapResources(self, usync,doc,idx,Map, maparchivecount):
 		resources = doc.createElement("MapResources")
 		resourceCount=usync.GetMapResourceCount(idx)
 		for i in range (0, resourceCount):
 			resource=doc.createElement("Resource")
-			getXmlData(doc, resource, "Name", str(usync.GetMapResourceName(idx, i)))
-			getXmlData(doc, resource, "Max", str(usync.GetMapResourceMax(idx, i)))
-			getXmlData(doc, resource, "ExtractorRadius", str(usync.GetMapResourceExtractorRadius(idx, i)))
+			self.getXmlData(doc, resource, "Name", str(usync.GetMapResourceName(idx, i)))
+			self.getXmlData(doc, resource, "Max", str(usync.GetMapResourceMax(idx, i)))
+			self.getXmlData(doc, resource, "ExtractorRadius", str(usync.GetMapResourceExtractorRadius(idx, i)))
 			resources.appendChild(resource)
 		Map.appendChild(resources)
 
-	def writeMapXmlData(usync, smap, idx, filename,maparchivecount,archivename):
-			if os.path.isfile(filename):
+	def writeMapXmlData(self, usync, smap, idx, filename,maparchivecount,archivename):
+		if os.path.isfile(filename):
 			print "[skip] " +filename + " already exists, skipping..."
 		else:
 			doc = minidom.Document()
 			archive = doc.createElement("Archive")
-			getXmlData(doc, archive, "Type", "Map")
+			self.getXmlData(doc, archive, "Type", "Map")
 			mapname=usync.GetMapName(idx)
-			getXmlData(doc, archive, "Name", mapname)
-			getXmlData(doc, archive, "Author", usync.GetMapAuthor(idx))
-			getXmlData(doc, archive, "Description", usync.GetMapDescription(idx))
-			getXmlData(doc, archive, "Gravity", str(usync.GetMapGravity(idx)))
-			getXmlData(doc, archive, "MaxWind", str(usync.GetMapWindMax(idx)))
-			getXmlData(doc, archive, "MinWind", str(usync.GetMapWindMin(idx)))
-					getXmlData(doc, archive, "TidalStrength", str(usync.GetMapTidalStrength(idx)))
+			self.getXmlData(doc, archive, "Name", mapname)
+			self.getXmlData(doc, archive, "Author", usync.GetMapAuthor(idx))
+			self.getXmlData(doc, archive, "Description", usync.GetMapDescription(idx))
+			self.getXmlData(doc, archive, "Gravity", str(usync.GetMapGravity(idx)))
+			self.getXmlData(doc, archive, "MaxWind", str(usync.GetMapWindMax(idx)))
+			self.getXmlData(doc, archive, "MinWind", str(usync.GetMapWindMin(idx)))
+			self.getXmlData(doc, archive, "TidalStrength", str(usync.GetMapTidalStrength(idx)))
 
-			getXmlData(doc, archive, "Height", str(usync.GetMapHeight(idx)))
-			getXmlData(doc, archive, "Width", str(usync.GetMapWidth(idx)))
+			self.getXmlData(doc, archive, "Height", str(usync.GetMapHeight(idx)))
+			self.getXmlData(doc, archive, "Width", str(usync.GetMapWidth(idx)))
 
-					getXmlData(doc, archive, "Gravity", str(usync.GetMapGravity(idx)))
-					getXmlData(doc, archive, "FileName", str(usync.GetMapFileName(idx)))
-					getXmlData(doc, archive, "MapMinHeight", str(usync.GetMapMinHeight(mapname)))
-					getXmlData(doc, archive, "MapMaxHeight", str(usync.GetMapMaxHeight(mapname)))
+			self.getXmlData(doc, archive, "Gravity", str(usync.GetMapGravity(idx)))
+			self.getXmlData(doc, archive, "FileName", str(usync.GetMapFileName(idx)))
+			self.getXmlData(doc, archive, "MapMinHeight", str(usync.GetMapMinHeight(mapname)))
+			self.getXmlData(doc, archive, "MapMaxHeight", str(usync.GetMapMaxHeight(mapname)))
 
-			getMapResources(usync, doc, idx,archive, maparchivecount)
+			self.getMapResources(usync, doc, idx,archive, maparchivecount)
 
-			getMapPositions(usync,doc,idx,archive)
-			getMapDepends(usync,doc,idx,archive,maparchivecount)
+			self.getMapPositions(usync,doc,idx,archive)
+			self.getMapDepends(usync,doc,idx,archive,maparchivecount)
 			doc.appendChild(archive)
 			tmp=".tmp.xml"
 			metadata = open(tmp,'w')
@@ -138,7 +142,7 @@ class Extract_metadata(UpqJob):
 			print "[created] " +filename +" ok"
 
 	# extracts minimap from given file
-	def createMapImage(usync, mapname, outfile, size):
+	def createMapImage(self, usync, mapname, outfile, size):
 		if os.path.isfile(outfile):
 			print "[skip] " +outfile + " already exists, skipping..."
 			return
@@ -150,7 +154,7 @@ class Extract_metadata(UpqJob):
 		shutil.move(tmp,outfile) # rename to dest
 		print "[created] " +outfile +" ok"
 
-	def createMapInfoImage(usync, mapname, maptype, byteperpx, decoder,decoderparm, outfile, size):
+	def createMapInfoImage(self, usync, mapname, maptype, byteperpx, decoder,decoderparm, outfile, size):
 		if os.path.isfile(outfile):
 			print "[skip] " +outfile + " already exists, skipping..."
 			return
@@ -172,7 +176,7 @@ class Extract_metadata(UpqJob):
 			print "[created] " +outfile +" ok"
 
 
-	def dumpmap(usync, springname, outpath, filename, idx):
+	def dumpmap(self, usync, springname, outpath, filename, idx):
 		metalmap = outpath + '/' + filename + ".metalmap" + ".jpg"
 		heightmap = outpath + '/' + filename + ".heightmap" + ".jpg"
 		mapimage = outpath + '/' + filename + ".jpg"
@@ -187,11 +191,11 @@ class Extract_metadata(UpqJob):
 				scaledsize=(1024, int(((mapheight/mapwidth) * 1024)))
 			else:
 				scaledsize=(int(((mapwidth/mapheight) * 1024)), 1024)
-			createMapImage(usync,springname,mapimage, scaledsize)
-			createMapInfoImage(usync,springname, "height",2, "RGB","BGR;15", heightmap, scaledsize)
-			createMapInfoImage(usync,springname, "metal",1, "L","L;I", metalmap, scaledsize)
+			self.createMapImage(usync,springname,mapimage, scaledsize)
+			self.createMapInfoImage(usync,springname, "height",2, "RGB","BGR;15", heightmap, scaledsize)
+			self.createMapInfoImage(usync,springname, "metal",1, "L","L;I", metalmap, scaledsize)
 
-	def getGameDepends(usync, idx, gamearchivecount, doc, game):
+	def getGameDepends(self, usync, idx, gamearchivecount, doc, game):
 		depends = doc.createElement("Depends")
 		game.appendChild(depends)
 		for i in range (1, gamearchivecount): # get depends for file, idx=0 is filename itself
@@ -202,9 +206,9 @@ class Extract_metadata(UpqJob):
 					depend=springnames[deps]
 				else:
 					depend=deps
-				getXmlData(doc, depends, "Depend", depend)
+				self.getXmlData(doc, depends, "Depend", depend)
 
-	def writeGameXmlData(usync, springname, idx, filename,gamesarchivecount, archivename):
+	def writeGameXmlData(self, usync, springname, idx, filename,gamesarchivecount, archivename):
 		if os.path.isfile(filename):
 			print "[skip] " +filename + " already exists, skipping..."
 			return
@@ -216,11 +220,11 @@ class Extract_metadata(UpqJob):
 			springname=springname[:len(springname)-len(version)]
 			if springname.endswith(" ") : #remove space at end (added through unitsync hack)
 				springname=springname[:len(springname)-1]
-		getXmlData(doc, archive, "Type", "Game")
-		getXmlData(doc, archive, "Name", springname)
-		getXmlData(doc, archive, "Description", usync.GetPrimaryModDescription(idx))
-		getXmlData(doc, archive, "Version", version)
-		getGameDepends(usync, idx, gamesarchivecount, doc, archive)
+		self.getXmlData(doc, archive, "Type", "Game")
+		self.getXmlData(doc, archive, "Name", springname)
+		self.getXmlData(doc, archive, "Description", usync.GetPrimaryModDescription(idx))
+		self.getXmlData(doc, archive, "Version", version)
+		self.getGameDepends(usync, idx, gamesarchivecount, doc, archive)
 		tmp=".tmp.xml"
 		f=open(tmp, 'w')
 		f.write(doc.toxml("utf-8"))
@@ -236,20 +240,20 @@ class Extract_metadata(UpqJob):
 	--datadir <maps/gamesdir>\n\
 	"
 	springnames={}
-	def createdict(usync,gamescount, mapcount):
+	def createdict(self,usync,gamescount, mapcount):
 		#create dict with springnames[filename]=springname
 		for i in range(0, gamescount):
 			springname=usync.GetPrimaryModName(i)
 			filename=usync.GetPrimaryModArchive(i)
-			springnames[filename]=springname
+			self.springnames[filename]=springname
 		for i in range(0, mapcount):
 			maparchivecount = usync.GetMapArchiveCount(usync.GetMapName(i)) # initialization for GetMapArchiveName()
 			filename = os.path.basename(usync.GetMapArchiveName(0))
 			print "["+str(i) +"/"+ str(mapcount)+ "] extracting data from "+filename
 			springname = usync.GetMapName(i)
-			springnames[filename]=springname
+			self.springnames[filename]=springname
 
-	def create_torrent(filename, output):
+	def create_torrent(self, filename, output):
 		if os.path.isdir(filename):
 			print "[skip] " +filename + "is a directory, can't create torrent"
 			return
