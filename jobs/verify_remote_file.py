9f4899eb641347ddac3ea2e60c01e44cb6fc42ef
abma <spam@abma.de>
2011-05-26 21:07:59 +0200

    more fixes for springfiles (yes it's live! :)

--- jobs/verify_remote_file.py
+++ jobs/verify_remote_file.py
@@ -31,8 +31,8 @@ class Verify_remote_file(upqjob.UpqJob):
     
     def run(self):
         # get files hash from DB
-        fid=int(self.jobdata['fmfid'])
-        result = upqdb.UpqDB().query("SELECT url_prefix, url_deamon, path, md5 FROM file_mirror_files AS f  LEFT JOIN file_mirror as m ON m.fmid=f.fmid WHERE fmfid=%d "%fid)
+        fmfid=int(self.jobdata['fmfid'])
+        result = upqdb.UpqDB().query("SELECT fid, url_prefix, url_deamon, path FROM file_mirror_files AS f  LEFT JOIN file_mirror as m ON m.fmid=f.fmid WHERE fmfid=%d "%fmfid)
         res = result.first()
 
         if not res:
@@ -43,21 +43,32 @@ class Verify_remote_file(upqjob.UpqJob):
         script_url = res['url_prefix']+"/"+res['url_deamon']
         params     = urllib.urlencode({'p': res['path']})
         hash_url   = script_url+"?%s"%params
+        file_path  = res['path']
 
+        result = upqdb.UpqDB().query("SELECT md5 FROM filehash WHERE fid=%d" %  int(res['fid']))
+        res = result.first()
+        if not res:
+            self.msg = "File md5 with jobdata='%s' not found in DB."%(self.jobdata)
+            self.logger.debug(self.msg)
+	    return False
+        md5 = res['md5']
         # retrieve md5 hash from deamon.php on remote mirror server
         self.logger.debug("retrieving '%s'", hash_url)
         hash_file = urllib.urlopen(hash_url)
         hash = hash_file.read()
 
-        self.msg = "received md5 hash for filename=%s on fmid=%s is %s" %(res['path'], fid, hash)
+        self.msg = "received md5 hash for filename=%s on fmfid=%s is %s" %(file_path, fmfid, hash)
         self.result = hash
         self.logger.debug(self.msg)
 
         
         if res['md5'] == hash:
+            upqdb.UpqDB().query("UPDATE file_mirror_files SET md5='%s', md5check=NOW(), active=1 WHERE fmfid=%d" %(res['md5'], fmfid))
             self.msg = 'Remote hash matches hash in DB.'
             return True
         else:
-			#TODO: delete from db to allow re-upload
+            #TODO: delete from db to allow re-upload
+            upqdb.UpqDB().query("UPDATE file_mirror_files SET md5='%s', active=0 WHERE fmfid=%d " %(hash, fmfid))
             self.msg  = 'Remote hash does NOT match hash in DB.'
             return False
+
