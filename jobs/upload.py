50727aedc7619b13d65c9a02f75dceac53430966
abma <spam@abma.de>
2011-05-30 10:31:10 +0200

    update todo

--- jobs/upload.py
+++ jobs/upload.py
@@ -19,6 +19,7 @@ import socket
 class Upload(UpqJob):
     def check(self):
          #TODO: check if file is already marked in db as uploaded
+         #TODO: reupload files with invalid md5
          #TODO: test + test +test + improve this!
          #TODO: create a job for each ftp-mirror here
          #TODO: create a cron-job or something similar, that creates upload jobs, when a new mirror is added (like a validation for all files/mirrors)
