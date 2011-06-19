3a8ad9d90e935b173b70ec08393b071877a1c455
abma <spring@abma.de>
2011-06-19 19:07:52 +0200

    cleanup database structure a bit + fix for sqlite

--- upqdb.py
+++ upqdb.py
@@ -40,9 +40,9 @@ class UpqDB():
 			results=self.query("SELECT sqlite_version()")
 		for res in results:
 			self.logger.info("SQL version: %s", res[0])
-	def connect(self, databaseurl):
+	def connect(self, databaseurl, debug):
 		self.cleanup()
-		self.engine = create_engine(databaseurl, encoding="utf-8", echo=False, pool_recycle=True)
+		self.engine = create_engine(databaseurl, encoding="utf-8", echo=debug, pool_recycle=True)
 		self.logger.info("Opened DB connection.")
 		self.meta=MetaData()
 		"""
@@ -65,87 +65,87 @@ class UpqDB():
 		pprint.pprint(meta.tables)
 		"""
 		self.tables['file_mirror']=Table('file_mirror', self.meta,
-			Column(u'fmid', INTEGER(display_width=10),  primary_key=True, nullable=False, autoincrement=True),
-			Column(u'title', VARCHAR(length=64)),
-			Column(u'description', TEXT()),
-			Column(u'country', VARCHAR(length=64)),
-			Column(u'ftp_url', VARCHAR(length=64)),
-			Column(u'ftp_user', VARCHAR(length=64)),
-			Column(u'ftp_pass', VARCHAR(length=64)),
-			Column(u'ftp_dir', VARCHAR(length=64)),
-			Column(u'ftp_passive', INTEGER(display_width=4)),
-			Column(u'ftp_ssl', INTEGER(display_width=4)),
-			Column(u'ftp_port', INTEGER(display_width=4)),
-			Column(u'url_prefix', VARCHAR(length=64)),
-			Column(u'url_deamon', VARCHAR(length=64)),
-			Column(u'uid', INTEGER(display_width=4)),
-			Column(u'mirror_size', INTEGER(display_width=11)),
-			Column(u'fill', BIGINT(display_width=20)),
-			Column(u'bandwidth_limit', INTEGER(display_width=11)),
-			Column(u'active', INTEGER(display_width=4)),
-			Column(u'flat', INTEGER(display_width=4)),
-			Column(u'dynamic', INTEGER(display_width=4)),
-			Column(u'mbw', BIGINT(display_width=20)),
-			Column(u'mbw_month', INTEGER(display_width=4)),
-			Column(u'tbw', BIGINT(display_width=20)),
-			Column(u'created', DATETIME(timezone=False)),
-			Column(u'changed', DATETIME(timezone=False)),
-			Column(u'lastupload', DATETIME(timezone=False),  nullable=False))
+			Column('fmid', INTEGER(display_width=10),  primary_key=True, nullable=False, autoincrement=True),
+			Column('title', VARCHAR(length=64)),
+			Column('description', TEXT()),
+			Column('country', VARCHAR(length=64)),
+			Column('ftp_url', VARCHAR(length=64)),
+			Column('ftp_user', VARCHAR(length=64)),
+			Column('ftp_pass', VARCHAR(length=64)),
+			Column('ftp_dir', VARCHAR(length=64)),
+			Column('ftp_passive', INTEGER(display_width=4)),
+			Column('ftp_ssl', INTEGER(display_width=4)),
+			Column('ftp_port', INTEGER(display_width=4)),
+			Column('url_prefix', VARCHAR(length=64)),
+			Column('url_deamon', VARCHAR(length=64)),
+			Column('uid', INTEGER(display_width=4)),
+			Column('mirror_size', INTEGER(display_width=11)),
+			Column('fill', BIGINT(display_width=20)),
+			Column('bandwidth_limit', INTEGER(display_width=11)),
+			Column('active', INTEGER(display_width=4)),
+			Column('flat', INTEGER(display_width=4)),
+			Column('dynamic', INTEGER(display_width=4)),
+			Column('mbw', BIGINT(display_width=20)),
+			Column('mbw_month', INTEGER(display_width=4)),
+			Column('tbw', BIGINT(display_width=20)),
+			Column('created', DATETIME(timezone=False)),
+			Column('changed', DATETIME(timezone=False)),
+			Column('lastupload', DATETIME(timezone=False),  nullable=False))
 		self.tables['file_mirror_files']=Table('file_mirror_files', self.meta,
-			Column(u'fmfid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=True),
-			Column(u'fmid', INTEGER(display_width=4)),
-			Column(u'fid', INTEGER(display_width=8)),
-			Column(u'path', VARCHAR(length=64)),
-			Column(u'size', INTEGER(display_width=11), nullable=False),
-			Column(u'md5', VARCHAR(length=64)),
-			Column(u'md5check', DATETIME(timezone=False)),
-			Column(u'active', INTEGER(display_width=4)),
-			Column(u'changed', DATETIME(timezone=False)))
+			Column('fmfid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=True),
+			Column('fmid', INTEGER(display_width=4)),
+			Column('fid', INTEGER(display_width=8)),
+			Column('path', VARCHAR(length=64)),
+			Column('size', INTEGER(display_width=11), nullable=False),
+			Column('md5', VARCHAR(length=64)),
+			Column('md5check', DATETIME(timezone=False)),
+			Column('active', INTEGER(display_width=4)),
+			Column('changed', DATETIME(timezone=False)))
 		self.tables['file_mirror_paths']=Table('file_mirror_paths', self.meta,
-			Column(u'fmpid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=True),
-			Column(u'fmid', INTEGER(display_width=4)),
-			Column(u'path', VARCHAR(length=64), nullable=False)),
+			Column('fmpid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=True),
+			Column('fmid', INTEGER(display_width=4)),
+			Column('path', VARCHAR(length=64), nullable=False)),
 		self.tables['filehash']=Table('filehash', self.meta,
-			Column(u'fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
-			Column(u'md5', CHAR(length=32)),
-			Column(u'sha1', CHAR(length=40)),
-			Column(u'sha256', CHAR(length=64)))
+			Column('fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
+			Column('md5', CHAR(length=32)),
+			Column('sha1', CHAR(length=40)),
+			Column('sha256', CHAR(length=64)))
 		self.tables['files']=Table('files', self.meta,
-			Column(u'fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=True),
-			Column(u'uid', INTEGER(display_width=10), nullable=False ),
-			Column(u'filename', VARCHAR(length=255), nullable=False),
-			Column(u'filepath', VARCHAR(length=255), nullable=False),
-			Column(u'filemime', VARCHAR(length=255), nullable=False),
-			Column(u'filesize', INTEGER(display_width=10), nullable=False, ),
-			Column(u'status', INTEGER(display_width=11), nullable=False),
-			Column(u'timestamp', INTEGER(display_width=10), nullable=False),
-			Column(u'origname', VARCHAR(length=255), nullable=False))
+			Column('fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=True),
+			Column('uid', INTEGER(display_width=10), nullable=False ),
+			Column('filename', VARCHAR(length=255), nullable=False),
+			Column('filepath', VARCHAR(length=255), nullable=False),
+			Column('filemime', VARCHAR(length=255), nullable=False),
+			Column('filesize', INTEGER(display_width=10), nullable=False, ),
+			Column('status', INTEGER(display_width=11), nullable=False),
+			Column('timestamp', INTEGER(display_width=10), nullable=False),
+			Column('origname', VARCHAR(length=255), nullable=False))
 		self.tables['springdata_archives']=Table('springdata_archives', self.meta,
-			Column(u'fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
-			Column(u'springname', VARCHAR(length=256)),
-			Column(u'cid', INTEGER(display_width=11)))
+			Column('fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
+			Column('springname', VARCHAR(length=256)),
+			Column('cid', INTEGER(display_width=11)))
 		self.tables['springdata_categories']=Table('springdata_categories', self.meta,
-			Column(u'cid', INTEGER(display_width=11), primary_key=True, nullable=False, autoincrement=True),
-			Column(u'name', VARCHAR(length=24), nullable=False))
+			Column('cid', INTEGER(display_width=11), primary_key=True, nullable=False, autoincrement=True),
+			Column('name', VARCHAR(length=24), nullable=False))
 		self.tables['springdata_depends']=Table('springdata_depends', self.meta,
-			Column(u'fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
-			Column(u'depends', INTEGER(display_width=10), primary_key=True, nullable=False),
-			Column(u'depends_string', VARCHAR(length=64), nullable=False))
+			Column('fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
+			Column('depends', INTEGER(display_width=10), primary_key=True, nullable=False),
+			Column('depends_string', VARCHAR(length=64), nullable=False))
 		self.tables['springdata_startpos']=Table('springdata_startpos', self.meta,
-			Column(u'fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
-			Column(u'id', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
-			Column(u'x', INTEGER(display_width=10), nullable=False),
-			Column(u'z', INTEGER(display_width=10), nullable=False))
+			Column('fid', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
+			Column('id', INTEGER(display_width=10), primary_key=True, nullable=False, autoincrement=False),
+			Column('x', INTEGER(display_width=10), nullable=False),
+			Column('z', INTEGER(display_width=10), nullable=False))
 		self.tables['upqueue']=Table('upqueue', self.meta,
-			Column(u'jobid', INTEGER(display_width=11), primary_key=True, nullable=False, autoincrement=True),
-			Column(u'jobname', VARCHAR(length=255), nullable=False),
-			Column(u'state', VARCHAR(length=32), nullable=False),
-			Column(u'jobdata', TEXT(), nullable=False),
-			Column(u'result', INTEGER(display_width=1), nullable=False),
-			Column(u'result_msg', VARCHAR(length=255), nullable=False, ),
-			Column(u'ctime', TIMESTAMP(timezone=False), nullable=False,),
-			Column(u'start_time', TIMESTAMP(timezone=False), nullable=False,),
-			Column(u'end_time', TIMESTAMP(timezone=False), nullable=False,))
+			Column('jobid', INTEGER(display_width=11), primary_key=True, nullable=False, autoincrement=True),
+			Column('jobname', VARCHAR(length=255), nullable=False),
+			Column('state', VARCHAR(length=32), nullable=False),
+			Column('jobdata', TEXT(), nullable=False),
+			Column('result', INTEGER(display_width=1)),
+			Column('result_msg', VARCHAR(length=255)),
+			Column('ctime', TIMESTAMP(timezone=False)),
+			Column('start_time', TIMESTAMP(timezone=False)),
+			Column('end_time', TIMESTAMP(timezone=False)))
 		try:
 			self.meta.create_all(self.engine)
 		except:
@@ -167,9 +167,12 @@ class UpqDB():
 		try:
 			s.execute(query)
 		except IntegrityError as e:
-			raise UpqDBIntegrityError("Integrity Error" + e.statement)
+			raise UpqDBIntegrityError("Integrity Error" + e.statement + str(values))
 		finally:
-			result=s.scalar("SELECT LAST_INSERT_ID()")
+			try:
+				result=s.scalar("SELECT LAST_INSERT_ID()")
+			except:
+				result=s.scalar("SELECT last_insert_rowid()")
 			s.close()
 			self.logger.debug("%s (%s) id:%s", query, values, result)
 		return result
