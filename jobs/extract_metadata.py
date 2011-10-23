97b66633fb08aba716857982d17f04f90df816d3
abma <spring@abma.de>
2011-10-23 12:24:43 +0200

    typo in extract_metadata

--- jobs/extract_metadata.py
+++ jobs/extract_metadata.py
@@ -162,7 +162,7 @@ class Extract_metadata(UpqJob):
 			idx=self.getGameIdx(usync,filename)
 			if idx<0:
 				self.logger.error("Invalid file detected: %s %s %s"% (filename,usync.GetNextError(), idx))
-				self.append_job("movefile", { status: 3 }) #mark file as broken
+				self.append_job("movefile", { "status": 3 }) #mark file as broken
 				return False
 			self.logger.debug("Extracting data from "+filename)
 			archivepath=usync.GetArchivePath(filename)+filename
