9478fa2e8d51e95d219b1a04b74ec62da6c6394f
abma <spring@abma.de>
2011-10-25 23:25:15 +0200

    allow * and ? as wildcard, % still works + cleanup

--- www/user.php
+++ www/user.php
@@ -1,7 +1,8 @@
 <html>
 <body>
 <table>
-<form>
+Input is case sensitive! Use * (multiple chars) or ? (single char) as wildcard. 10 Results are returned at max.
+<form action="user.php" method="get">
 	<tr>
 		<tr><td>Springname:</td>
 		<td><input type="text" name="springname" value="<?php if (array_key_exists('springname', $_REQUEST)) echo $_REQUEST['springname']; ?>"></td>
@@ -21,37 +22,11 @@ require("config.php");
 require("include/drupal_dummy.inc");
 require("include/search.inc");
 
-function json_search($req){
-	$res=search($req);
-	for($i=0; $i<count($res); $i++){
-		if(array_key_exists('torrent', $res[$i])){
-			$res[$i]['torrent']=base64_encode($res[$i]['torrent']);
-		}
-	}
-	return $res;
-}
-
-function main($argv){
-	$req=array(
-		"springname"=>"%",
-		"torrent"=>"true"
-	);
-	$res=json_search($req);
-	echo json_encode($res);
-}
-
-if (array_key_exists('argv', $_SERVER))
-	main($_SERVER['argv']);
-else if ($_SERVER['REQUEST_METHOD']=="GET"){
-	$res=json_search($_REQUEST);
-} else {
-	echo file_get_contents("readme.html");
-}
 /**
  *	format a result to a human readable output
  **/
 function dump_result($res, $rec=0){
-	$space="\t";
+	$space="";
 	for($i=0;$i<$rec; $i++)
 		$space=$space."\t";
 	if (is_array($res)){
@@ -70,6 +45,7 @@ function dump_result($res, $rec=0){
 	}
 }
 
+$res=search($_REQUEST);
 echo "<pre>";
 echo dump_result($res);
 echo "</pre>";
