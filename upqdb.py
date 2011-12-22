dd76a14df1c46ce15f8fa475ed854b3bb16e9d5a
abma <spring@abma.de>
2011-12-22 23:02:30 +0100

    made sdp unique in db

--- upqdb.py
+++ upqdb.py
@@ -104,7 +104,7 @@ class UpqDB():
 			Column('name', VARCHAR(length=256)), #spring name of this file
 			Column('version', VARCHAR(length=256)), #spring version of this file
 			Column('cid', INTEGER(display_width=11)), #category of this file: game/map
-			Column('sdp', VARCHAR(length=32),unique=True), #for this file
+			Column('sdp', VARCHAR(length=32),nullable=False, unique=True), #for this file
 			Column('descriptionuri', VARCHAR(length=1024)),
 			Column('metadata', TEXT()),
 			UniqueConstraint('name', 'version'))
