84363df9dfc7c6e1bf1dffcee5e24fd58cedb1ac
abma <spring@abma.de>
2011-12-30 02:58:30 +0100

    update table structure (include timestamp)

--- www/dbsetup.sql
+++ www/dbsetup.sql
@@ -1,9 +1,9 @@
 
-CREATE TABLE `upq`.`xmlrpc_log` (
-`id` INT NOT NULL AUTO_INCREMENT ,
-`ip` VARCHAR( 16 ) NOT NULL ,
-`method` VARCHAR( 32 ) NOT NULL ,
-`data` TEXT NOT NULL ,
-PRIMARY KEY ( `id` )
-) ENGINE = MYISAM ;
-
+CREATE TABLE IF NOT EXISTS `xmlrpc_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(16) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `method` varchar(32) NOT NULL,
+  `data` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM;
