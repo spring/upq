af27f529e99917f847de309e2575e4b3964f834d
abma <spring@abma.de>
2014-11-16 14:33:05 +0100

    rotate logfiles

--- run.sh
+++ run.sh
@@ -4,6 +4,9 @@ if [ -e ~/lib/python ]; then
 fi
 cd $(dirname $0)
 #kill remaining processes
+mv upq.log.2 upq.log.3
+mv upq.log.1 upq.log.2
+mv upq.log upq.log.1
 pkill -u $(whoami) -f "python upq.py"
 exec nice -19 ionice -c3 python upq.py
 
