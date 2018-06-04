104847547629e4ca82f509ebee1c5f2f7f579dc0
abma <spring@abma.de>
2018-06-04 19:27:50 +0200

    xmlrpc -> json api + https

--- doc/exampleclient.py
+++ doc/exampleclient.py
@@ -1,26 +1,12 @@
 #!/usr/bin/env python
 
-from xmlrpclib import ServerProxy
-import pprint
-import sys
+import requests
 
-if len(sys.argv) < 2:
-	print "Usage: "+sys.argv[0]+" FilenameToSearchFor"
-	exit(0)
-
-proxy = ServerProxy('http://api.springfiles.com/xmlrpc.php', verbose=True)
-searchstring = {
-	"category" : "map",
-	"logical" : "or",
-	"tag" : sys.argv[1],
-	"filename" : sys.argv[1],
-	"springname" : sys.argv[1],
-	"torrent" : True,
-	"metadata" : True,
-	"nosensitive" : True,
-	"images" : True
+searchparams = {
+	"category" : "engine_linux64",
+	"limit": 1
 }
 
-pp = pprint.PrettyPrinter(depth=6)
-pp.pprint(proxy.springfiles.search(searchstring))
+r = requests.get('https://api.springfiles.com/json.php', params=searchparams)
 
+print(r.json())
