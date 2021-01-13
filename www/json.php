aeab7f83a602d8b5988b2f87f29fc81773611776
abma <spring@abma.de>
2021-01-13 23:43:07 +0100

    add param 'callback' / allow to return jsonp: fix #67

--- www/json.php
+++ www/json.php
@@ -22,11 +22,22 @@ function main($argv){
 	echo json_encode($res);
 }
 
+function JsonResult($request)
+{
+	return json_encode(json_search($request));
+}
+
 if (array_key_exists('argv', $_SERVER))
 	main($_SERVER['argv']);
 else
 	if ($_SERVER['REQUEST_METHOD']=="GET"){
-		echo json_encode(json_search($_REQUEST));
+		$cb = $_REQUEST['callback'];
+		if (isset($cb) && !empty($cb)) {
+			$cb = preg_replace('/[^a-zA-Z0-9]/', '', $cb); // strip anything except a-Z0-9
+			echo ($cb .'(' . JsonResult($_REQUEST) . ');');
+		} else {
+			echo JsonResult($_REQUEST);
+		}
 	} else {
 		echo file_get_contents("readme.html");
 		return;
