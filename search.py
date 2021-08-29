e4f4e27e60fe69d2de4b2d61fcb7170d20e7bc3e
abma <spring@abma.de>
2021-08-30 01:19:04 +0200

    rewrote all php code in python (untested)

--- search.py
+++ search.py
@@ -1,4 +1,6 @@
 from lib import upqdb, upqconfig
+import json
+import random
 
 cfg = upqconfig.UpqConfig()
 cfg.readConfig()
@@ -46,29 +48,34 @@ def GetQuery(request, binary, tag, cond):
 		"binary": binary,
 		tag : sqlescape(request[tag])
 	}
-	print(params)
+	#print(params)
 	return [cond.format(**params)]
+
+conditions = {
+	"tag": "t.tag LIKE {binary} '{tag}'",
+	"filename": "f.filename LIKE {binary} '{filename}'",
+	"category": "c.name LIKE {binary} '{category}'",
+	"name": "f.name LIKE {binary} '{name}'",
+	"version": "f.version LIKE {binary}'{version}'",
+	"sdp": "f.sdp LIKE {binary} '{sdp}'",
+	"springname": "( (CONCAT(f.name,' ',f.version) LIKE {binary} '{springname}') OR (f.name LIKE {binary} '{springname}' OR f.version LIKE {binary} '{springname}') )",
+	"md5": "f.md5 = '{md5}'",
+}
+
 
 
 wheres = []
-wheres += GetQuery(request, binary, "tag", "t.tag LIKE {binary} '{tag}'")
-wheres += GetQuery(request, binary, "filename", "f.filename LIKE {binary} '{filename}'")
-wheres += GetQuery(request, binary, "category", "c.name LIKE {binary} '{category}'")
-wheres += GetQuery(request, binary, "name", "f.name LIKE {binary} '{name}'");
-
-wheres += GetQuery(request, binary, "version", "f.version LIKE {binary}'{version}'")
-wheres += GetQuery(request, binary, "sdp", "f.sdp LIKE {binary} '{sdp}'")
-wheres += GetQuery(request, binary, "springname", "( (CONCAT(f.name,' ',f.version) LIKE {binary} '{springname}') OR (f.name LIKE {binary} '{springname}' OR f.version LIKE {binary} '{springname}') )")
-wheres += GetQuery(request, binary, "md5", "f.md5 = '{md5}'")
-
+for tag, condition in conditions.items():
+	wheres += GetQuery(request, binary, tag, condition)
 
 wherecond = logical.join(wheres)
 if wherecond:
 	wherecond = " AND " + wherecond
 
-print(wherecond)
 
-res = db.query("""SELECT
+#print(wherecond)
+
+rows = db.query("""SELECT
 distinct(f.fid) as fid,
 f.name as name,
 f.filename as filename,
@@ -90,119 +97,57 @@ ORDER BY f.timestamp DESC
 %s
 """%(wherecond, getlimit(request)))
 
+def GetMirrors(db, fid):
+	rows = db.query("""SELECT CONCAT(m.url_prefix,"/",mf.path) as url
+		FROM mirror_file as mf
+		LEFT JOIN mirror as m ON mf.mid=m.mid
+		LEFT JOIN file as f ON f.fid=mf.fid
+		WHERE f.fid=%d
+		AND (m.status=1 or m.status=2)
+		AND mf.status=1""" % fid);
 
-for row in res:
-	print(row)
+	res = []
+	for row in rows:
+		res.append(row[0])
+	random.shuffle(res)
+	return res
 
-"""
-/**
-* return images
-*/
-function _get_metadata($row){
-	$meta=json_decode($row, true);
-	if (is_array($meta))
-		return $meta;
-	return array();
-}
-
-$https=(!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') || (!empty($_SERVER['SERVER_PORT']) && $_SERVER['SERVER_PORT'] == 443);
-function TransformUrl($url)
-{
-	global $https;
-	if ($https) {
-		return str_replace("http://", "https://", $url);
-	}
-	return $url;
-}
-
-function _add_metadata_path($arr){
-	global $config;
-	$res=array();
-	foreach ($arr as $val){
-		$res[] = TransformUrl($config['metadata'].'/'.$val);
-	}
-	return $res;
-	
-}
-
-/**
-*	implementation of the xml-rpc call
-*/
-function search($req){
-	$res=array();
-	while($row = db_fetch_array($result)){
-		#inject local file as mirror
-		if (($row['category'] == "game") or ($row['category'] == "map")) {
-			$row['mirrors']=TransformUrl(array($config['base_url'].'/'.$row['path'].'/'.$row['filename']));
-		} else {
-			$row['mirrors']=array();
-		}
-		$res[]=$row;
-	}
+def GetMetadataPaths(images):
+	res = []
+	for image in images:
+		res.append("https://springfiles.springrts.com/metadata/"  + image)
+	return res
 
 
-	for($i=0;$i<count($res);$i++){
-		//search + add depends to file
-		$result=db_query("SELECT CONCAT(a.name,' ',a.version) as springname, depends_string
-			FROM {file_depends} AS d
-			LEFT JOIN {file} AS a ON d.depends_fid=a.fid
-			WHERE d.fid=%d",$res[$i]['fid']);
-		while($row = db_fetch_array($result)){
-			if(!array_key_exists('depends', $res[$i]))
-				$res[$i]['depends']=array();
-			if(strlen($row['springname'])<=0) // 0 means it wasn't resolveable, return the string
-				$res[$i]['depends'][]=$row['depends_string'];
-			else{ //is resolveable, return string from archive
-				$res[$i]['depends'][]=$row['springname'];
-			}
-		}
-		//search + add mirrors to file
-		$result=db_query('SELECT CONCAT(m.url_prefix,"/",mf.path) as url
-			FROM mirror_file as mf
-			LEFT JOIN mirror as m ON mf.mid=m.mid
-			LEFT JOIN file as f ON f.fid=mf.fid
-			WHERE f.fid=%d
-			AND (m.status=1 or m.status=2)
-			AND mf.status=1',array($res[$i]['fid']));
-		while($row = db_fetch_array($result)){
-			$res[$i]['mirrors'][]=TransformUrl($row['url']);
-		}
-		//randomize order of result
-		shuffle($res[$i]['mirrors']);
+clientres = []
 
-		//add tags
-		$res[$i]['tags']=array();
-		$result=db_query('SELECT tag FROM {tag} t
-			WHERE fid=%d', array($res[$i]['fid']));
-		while($row = db_fetch_array($result)){
-			$res[$i]['tags'][]=$row['tag'];
-		}
-		$metadata=_get_metadata($res[$i]['metadata']);
-		if (array_key_exists('images', $req)){
-			if (array_key_exists('splash', $metadata))
-				$res[$i]['splash'] = _add_metadata_path($metadata['splash']);
-			if (array_key_exists('mapimages', $metadata))
-				$res[$i]['mapimages'] = _add_metadata_path($metadata['mapimages']);
-		}			
-		unset($metadata['splash']);
-		unset($metadata['mapimages']);
-		//links to metadata
-		if(array_key_exists('metadata', $req)){
-			$res[$i]['metadata']=$metadata;
-		}else{
-			unset($res[$i]['metadata']);
-		}
+for row in rows:
+	d = dict(row)
+	#inject local file as mirror
+	if d["category"] in ["game", "map"]:
+		d["mirrors"] = ["https://springfiles.springrts.com/" + d["path"] + d["filename"]]
+	else:
+		d["mirrors"] = []
 
-		//additional metadata
-		$res[$i]['size']=intval($res[$i]['size']);
-		if ($res[$i]['version']=="")
-			$res[$i]['springname']=$res[$i]['name'];
-		else
-			$res[$i]['springname']=$res[$i]['name']." ".$res[$i]['version'];
-		unset($res[$i]['fid']);
-		unset($res[$i]['path']);
-		ksort($res[$i]);
-	}
-	return $res;
-}
-"""
+	d["mirrors"] += GetMirrors(db, d["fid"])
+	#print(mirrors)
+	#print(row)
+
+	if "images" in request:
+		for k in ["splash", "mapimages"]:
+			if k in d["metadata"]:
+				d[k] = GetMetadataPaths(d["metadata"][k])
+				del(d["metadata"][k])
+
+	if not "metadata" in request:
+		del(d["metadata"])
+
+	#if "splash" in request:
+	#if "images" in request:
+
+	#json.dumps(row)
+	del(d["fid"])
+	d["timestamp"] = d["timestamp"].isoformat()
+	clientres.append(d)
+
+json.dumps(clientres)
