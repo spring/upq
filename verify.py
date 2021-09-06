193760d8bff40864391376f96ed89d41b5111d59
abma <spring@abma.de>
2021-09-06 22:30:32 +0200

    fix verify diff

--- verify.py
+++ verify.py
@@ -29,6 +29,23 @@ for path in os.path.join(cfg.paths["files"], "maps"),  os.path.join(cfg.paths["f
 	for f in os.listdir(path):
 		files.add(os.path.join(path, f))
 
+#assert("alphasiegedry_2.2.sd7" in dbfiles)
+
 diff = dbfiles.difference(files)
 if diff:
-	raise Exception("db vs filesystem missmatch: " %str(diff))
+	raise Exception("db vs filesystem missmatch: %s" %str(diff))
+
+def AddFilesToDB(files):
+	from lib import log, upqdb, extract_metadata
+	db = upqdb.UpqDB(cfg.db['url'], cfg.db['debug'])
+
+	for f in files:
+		extract_metadata.Extract_metadata(cfg, db, f, 1)
+
+diff = files.difference(dbfiles)
+if diff:
+	raise Exception("db vs filesystem missmatch: %s" %str(diff))
+	#AddFilesToDB(diff)
+
+#AddFilesToDB(["/home/springfiles/upq/files/maps/alphasiegedry_2.2.sd7"])
+
